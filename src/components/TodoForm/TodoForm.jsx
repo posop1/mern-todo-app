1363d5a41165eff16fbbbd92a2a04de682d90673
Artur Yakhin <artur.yakhin52@gmail.com>
2022-10-09 12:54:01 +0500

    feat: todo from desing

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,11 +1,15 @@
 import styles from './TodoForm.module.scss';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const TodoForm = () => {
   return (
     <form className={styles.todoForm}>
       <input type="text" placeholder="Task title..." className={styles.form__input} />
       <input type="text" placeholder="Task discription..." className={styles.form__input} />
-      <button className={styles.form__button}>Add New Task</button>
+      <button className={styles.form__button}>
+        <AiOutlinePlus className={styles.icon} />
+        Add New Task
+      </button>
     </form>
   );
 };
